b6e4fc500df312c89e8925a0866e7b2e3ccbe35b
Yexzari <20203tn141@utez.edu.mx>
2024-02-28 19:22:27 -0600

    fc

---
+++ src/main/java/mx/edu/utez/sda/d8cspringmvc/config/WebConfig.java
@@ -0,0 +1,18 @@
+package mx.edu.utez.sda.d8cspringmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/form").setViewName("form");
+    }
+}
